c2c90b72cd197e5c4bb935be73d6a712fa6dd156
Sukhesh-garg <gargsukhesh7@gmail.com>
2025-09-04 21:47:37 +0530

    ques3.cpp

--- ques3.cpp
+++ ques3.cpp
@@ -4,7 +4,7 @@ int main()
 {
     int arr[] = {1, 2, 3, 5, 6, 7}; // 4 is missing
     int n = 6;
-    int expectedSum = ((arr[0] + arr[n - 1]) * (n + 1)) / 2;
+    int expectedSum = ((n+2) * (n + 1)) / 2;
     int actualSum = 0;
     for (int i = 0; i < n; i++)
         actualSum += arr[i];
